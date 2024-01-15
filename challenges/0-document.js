1692f8fe5a0a23b7fd0f33cb3e1006c67d2d66ac
LaurenAnds <laurenjayne.ands@outlook.com>
2024-01-15 13:06:02 +0000

    first attempt at documenting

--- challenges/0-document.js
+++ challenges/0-document.js
@@ -1,5 +1,11 @@
 // TODO (Ex.0): document and write tests for this function
 
+/**
+ * Return a negative version of a given number.
+ * @param {number} inputNum - the given number to be made negative.
+ * @returns {number} - the result - a negative version of the given number.
+ */
+
 function asNegative(n) {
   // ...
 }
